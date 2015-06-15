32d6a196df8593915c1a7e9de3869cfceac775c1
Kirsten Korevaar <kirsten@eml.cc>
2015-06-15 13:24:45 -0400

    Add placeholder functions

--- phonebook.py
+++ phonebook.py
@@ -5,6 +5,22 @@ def create_phonebook(phonebook_name):
     """Create a new phone book file if it does not exist already."""
     pass
 
+def add_entry(name, number, phonebook_name):
+    # add a new name and number to the given phonebook
+    pass
+
+def update(name, number, phonebook_name):
+    pass
+
+def lookup(name, phonebook_name):
+    pass
+
+def reverse_loopup(number, phonebook_name):
+    pass 
+
+def delete(name, phonebook_name):
+    pass    
+
 
 if __name__ == "__main__":
     try:
